f7f46a50dde9f88804107da7c83773c6f154a02a
Francisco Aires <xicojr@gmail.com>
2025-08-28 20:43:24 -0300

    Envio de email - finalizado 3

--- netlify/functions/db.js
+++ netlify/functions/db.js
@@ -1,9 +1,10 @@
-// db.ts
+// db.js
 import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
